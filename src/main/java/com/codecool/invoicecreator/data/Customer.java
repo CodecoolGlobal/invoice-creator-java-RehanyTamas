7339391b136a0396be0079099dd258b39aa65b8e
RehanyTamas <rehany.tamas14@gmail.com>
2023-02-10 23:43:07 +0100

    customer class addded

---
+++ src/main/java/com/codecool/invoicecreator/data/Customer.java
@@ -0,0 +1,9 @@
+package com.codecool.invoicecreator.data;
+
+public class Customer {
+    private  int id;
+    private  String name;
+    private  String email;
+    private String address;
+    private  int tax_number;
+}
